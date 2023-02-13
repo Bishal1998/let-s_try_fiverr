137d9f355eb21fc132e182783f712919f356b66f
Bishal1998 <Bishalpaudel1998@gmail.com>
2023-02-13 14:59:40 +0545

    navbar design

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Navbar.scss'
 
 const Navbar = () => {
+
+    const [active, setActive] = useState(false)
+
+    const isActive = () => {
+        window.scrollY > 0 ? setActive(true) : setActive(false);
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", isActive);
+
+        return () => {
+            window.removeEventListener("scroll", isActive);
+        }
+    }, [])
+
     return (
-        <div className='navbar'>Navbar</div>
+        <div className={active ? 'navbar active' : 'navbar'}>
+            <div className="container">
+                <div className="logo">
+                    <span className="text">fiverr</span>
+                    <span className="dot">.</span>
+                </div>
+                <div className="links">
+                    <span>Fiverr Business</span>
+                    <span>Explore</span>
+                    <span>English</span>
+                    <span>USD</span>
+                    <span>Become a Seller</span>
+                    <span>Sign in</span>
+                    <button>Join</button>
+                </div>
+            </div>
+            {
+                active && (
+                    <>
+                        <hr />
+                        <div className="menu">
+                            <span>Test1</span>
+                            <span>Test2</span>
+                        </div>
+                    </>
+                )
+            }
+        </div>
     )
 }
 
